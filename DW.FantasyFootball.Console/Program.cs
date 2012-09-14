de7a7c249da8da9b35b39166089ad6e2a6723424
Douglas Waugh <douglas.waugh.e3@gmail.com>
2012-09-14 09:14:38 +0100

    Pre-github push commit

--- DW.FantasyFootball.Console/Program.cs
+++ DW.FantasyFootball.Console/Program.cs
@@ -29,7 +29,8 @@ namespace DW.FantasyFootball.Console
                         DownloadFixtures();
                         break;
                     case 'b':
-                        SuggestPlayerToBuy();
+                        var suggestor = new PlayerSuggestor();
+                        System.Console.WriteLine(suggestor.SuggestPlayerToBuy().ToString());
                         break;
                     default:
                         var fixtureometer = new Fixtureometer(new FixtureScrapper());
@@ -49,6 +50,7 @@ namespace DW.FantasyFootball.Console
         private static void SuggestPlayerToBuy()
         {
             Player player = null;
+
             System.Console.WriteLine("You should buy: {0}", player);
         }
 
@@ -135,90 +137,11 @@ namespace DW.FantasyFootball.Console
         }
     }
 
-    public class Player
+    public class PlayerSuggestor
     {
-    }
-
-    public class Fixtureometer
-    {
-        private readonly FixtureScrapper _fixtureScrapper;
-
-        public Fixtureometer(FixtureScrapper fixtureScrapper)
+        public Player SuggestPlayerToBuy()
         {
-            _fixtureScrapper = fixtureScrapper;
-        }
-
-        public void CalculateFixtureStrength(ILog _logger)
-        {
-            var fixtureList = _fixtureScrapper.GetLeague();
-
-            var league = new League(fixtureList);
-
-            var stats = new Stats();
-
-            foreach (var teamData in league)
-            {
-                var nextFixtures = fixtureList.GetNextFixtures(teamData.Key, 11);
-
-                foreach (var nextFixture in nextFixtures)
-                {
-                    if (nextFixture == null)
-                        break;
-
-                    var homeFixtures = fixtureList.GetLastXHomeFixturesForTeam(nextFixture.HomeTeam, 6);
-
-                    var awayFixtures = fixtureList.GetLastXAwayFixturesForTeam(nextFixture.AwayTeam, 6);
-
-                    var expectedHomeGoals = (homeFixtures.Average(f => (decimal)f.HomeGoals) + awayFixtures.Average(f => (decimal)f.HomeGoals)) / 2m;
-
-                    var expectedAwayGoals = (awayFixtures.Average(f => (decimal)f.AwayGoals) + homeFixtures.Average(f => (decimal)f.AwayGoals)) / 2m;
-
-                    if (nextFixture.HomeTeam == teamData.Key)
-                    {
-                        stats.AddFutureFixture(nextFixture.HomeTeam, new StatFixture(expectedHomeGoals, expectedAwayGoals));
-                    }
-                    else
-                    {
-                        stats.AddFutureFixture(nextFixture.AwayTeam, new StatFixture(expectedAwayGoals, expectedHomeGoals));
-                    }
-
-                    /*System.Console.WriteLine(
-                            string.Format("{0} v {1} for {2} against {3}",
-                            nextFixture.HomeTeam,
-                            nextFixture.AwayTeam,
-                            expectedHomeGoals.ToString("{0.00}"),
-                            expectedAwayGoals.ToString("{0.00}"))
-                        );*/
-                }
-            }
-
-            System.Console.WriteLine("\nDefensive Stats");
-
-            foreach (var stat in stats.OrderedByProbabilityOfAtLeastOneCleanSheet())
-            {
-                System.Console.WriteLine(stat.Key.Name + " " + stat.Value.ProbabilityOfAtLeastOneCleanSheet);
-            }
-
-            System.Console.WriteLine("\nOffensive Stats Total");
-
-            foreach (var stat in stats.OrderedByOffensivePointsTotal())
-            {
-                System.Console.WriteLine(stat.Key.Name + " " + stat.Value.OffensivePointsTotal);
-            }
-
-            System.Console.WriteLine("\nOffensive Stats Average");
-
-            foreach (var stat in stats.OrderedByOffensivePointsAverage())
-            {
-                System.Console.WriteLine(stat.Key.Name + " " + stat.Value.OffensivePointsAverage);
-            }
-
-            System.Console.WriteLine("\nGames Remaining");
-
-            foreach (var stat in stats)
-            {
-                System.Console.WriteLine(stat.Key.Name + " " + stat.Value.Count());
-            }
+            return new Player();
         }
     }
 }
